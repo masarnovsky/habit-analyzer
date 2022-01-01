ff3e2f1752bb5009908991685ee63070c514a4f6
m.masarnovsky <m.masarnovsky@gmail.com>
2022-01-01 23:44:27 +0300

    init commit

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.5.10"
+    application
+}
+
+group = "me.masarnovsky"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+
+    implementation("org.apache.commons:commons-csv:1.8")
+//    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("MainKt")
+}
